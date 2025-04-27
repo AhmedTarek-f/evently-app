c5d8388b70b8a5ca0742adc6b2b69665b2ea412c
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-04-27 21:46:28 +0300

    adding app theme and screen utils

--- lib/evently_app.dart
+++ lib/evently_app.dart
@@ -1,17 +1,22 @@
+import 'package:evently_app/core/utils/app_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class EventlyApp extends StatelessWidget {
   const EventlyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return ScreenUtilInit(
+      designSize: const Size(393, 841),
+      child: MaterialApp(
+        title: 'Evently',
+        debugShowCheckedModeBanner: false,
+        themeMode: AppTheme.appTheme,
+        theme: AppTheme.lightTheme,
+        darkTheme: AppTheme.darkTheme,
+        home: const Placeholder(),
       ),
-      home: const Placeholder(),
     );
   }
 }
