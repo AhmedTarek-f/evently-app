c8a54f823662ccedd5d057361c7b3af16f18ab0b
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-04-27 21:45:13 +0300

    adding app images

---
+++ lib/core/constants/app_images.dart
@@ -0,0 +1,11 @@
+abstract final class AppImages {
+  static const String startScreenLight = "assets/images/start_screen_light.png";
+  static const String startScreenDark = "assets/images/start_screen_dark.png";
+  static const String lightOnboarding1 = "assets/images/light_onboarding_1.png";
+  static const String lightOnboarding2 = "assets/images/light_onboarding_2.png";
+  static const String lightOnboarding3 = "assets/images/light_onboarding_3.png";
+  static const String darkOnboarding1 = "assets/images/dark_onboarding_1.png";
+  static const String darkOnboarding2 = "assets/images/dark_onboarding_2.png";
+  static const String darkOnboarding3 = "assets/images/dark_onboarding_3.png";
+  static const String eventlyLogo = "assets/images/evently_logo.png";
+}
