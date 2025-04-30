92728741d7e09a753af11e0c27068277fa031333
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-04-30 22:54:25 +0300

    adding onboarding model

---
+++ lib/features/onboarding/data/models/onboarding_model.dart
@@ -0,0 +1,10 @@
+class OnboardingModel {
+  const OnboardingModel({
+    required this.image,
+    required this.title,
+    required this.subTitle,
+  });
+  final String image;
+  final String title;
+  final String subTitle;
+}
