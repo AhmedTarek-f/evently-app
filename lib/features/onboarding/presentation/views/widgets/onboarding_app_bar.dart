0b66b805118a88c090c7991d9558f3829d6e50fd
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-04-30 22:54:09 +0300

    adding onboarding app bar

---
+++ lib/features/onboarding/presentation/views/widgets/onboarding_app_bar.dart
@@ -0,0 +1,18 @@
+import 'package:evently_app/core/common_widgets/evently_logo.dart';
+import 'package:flutter/material.dart';
+
+class OnboardingAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const OnboardingAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: const EventlyLogo(),
+      elevation: 0,
+      surfaceTintColor: Colors.transparent,
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
