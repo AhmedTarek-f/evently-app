0c4efe14ef212421e2636b56d3317ebd163883d0
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-04-30 19:35:45 +0300

    adding app icons

---
+++ lib/core/constants/app_icons.dart
@@ -0,0 +1,6 @@
+abstract final class AppIcons {
+  static const String eg = "assets/icons/eg.svg";
+  static const String us = "assets/icons/us.svg";
+  static const String moon = "assets/icons/moon.svg";
+  static const String sun = "assets/icons/sun.svg";
+}
