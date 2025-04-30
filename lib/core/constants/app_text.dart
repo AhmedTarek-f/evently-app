f552385fbb465be45a580338e07604e7fbd6cbb3
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-04-30 22:57:30 +0300

    adding onboarding text

--- lib/core/constants/app_text.dart
+++ lib/core/constants/app_text.dart
@@ -5,4 +5,13 @@ abstract final class AppText {
   static const String language = "language";
   static const String theme = "theme";
   static const String startButton = "startButton";
+  static const String onBoardingTitle1 = "Find Events That Inspire You";
+  static const String onBoardingTitle2 = "Effortless Event Planning";
+  static const String onBoardingTitle3 = "Connect with Friends & Share Moments";
+  static const String onBoardingSubTitle1 =
+      "Dive into a world of events crafted to fit your unique interests. Whether you're into live music, art workshops, professional networking, or simply discovering new experiences, we have something for everyone. Our curated recommendations will help you explore, connect, and make the most of every opportunity around you.";
+  static const String onBoardingSubTitle2 =
+      "Take the hassle out of organizing events with our all-in-one planning tools. From setting up invites and managing RSVPs to scheduling reminders and coordinating details, we’ve got you covered. Plan with ease and focus on what matters – creating an unforgettable experience for you and your guests.";
+  static const String onBoardingSubTitle3 =
+      "Make every event memorable by sharing the experience with others. Our platform lets you invite friends, keep everyone in the loop, and celebrate moments together. Capture and share the excitement with your network, so you can relive the highlights and cherish the memories.";
 }
