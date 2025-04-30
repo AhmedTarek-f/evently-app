c6abb57ae10a3583601c0366de83b08a6d00a937
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-04-30 22:57:14 +0300

    adding onboarding app icons

--- lib/core/constants/app_icons.dart
+++ lib/core/constants/app_icons.dart
@@ -3,4 +3,12 @@ abstract final class AppIcons {
   static const String us = "assets/icons/us.svg";
   static const String moon = "assets/icons/moon.svg";
   static const String sun = "assets/icons/sun.svg";
+  static const String darkLeftCircularArrow =
+      "assets/icons/dark_left_circular_arrow.svg";
+  static const String darkRightCircularArrow =
+      "assets/icons/dark_right_circular_arrow.svg";
+  static const String lightLeftCircularArrow =
+      "assets/icons/light_left_circular_arrow.svg";
+  static const String lightRightCircularArrow =
+      "assets/icons/light_right_circular_arrow.svg";
 }
