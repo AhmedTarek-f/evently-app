aae520e7e541c44c3f778765f54a5ac18c6196a0
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-04-30 19:41:03 +0300

    adding native splash, localization, and providing start cubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,29 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:evently_app/core/cache/shared_preferences_helper.dart';
 import 'package:evently_app/evently_app.dart';
+import 'package:evently_app/features/start/presentation/views_model/start_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
-void main() {
-  runApp(const EventlyApp());
+void main() async {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  await SharedPreferencesHelper.init();
+  await EasyLocalization.ensureInitialized();
+  FlutterNativeSplash.remove();
+  runApp(
+    BlocProvider<StartCubit>(
+      create: (context) => StartCubit(),
+      child: EasyLocalization(
+        supportedLocales: const [
+          Locale('en'),
+          Locale('ar'),
+        ],
+        path: 'assets/translations',
+        fallbackLocale: const Locale('en'),
+        child: const EventlyApp(),
+      ),
+    ),
+  );
 }
