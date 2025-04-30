fb3df94d5bdc664d499dfe2559f7bf455c1037d0
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-04-30 19:43:37 +0300

    adding new colors and text

--- lib/core/utils/app_theme.dart
+++ lib/core/utils/app_theme.dart
@@ -3,46 +3,21 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 abstract class AppTheme {
-  static ThemeMode appTheme = ThemeMode.light;
-
   static ThemeData lightTheme = ThemeData(
-    useMaterial3: true,
-    scaffoldBackgroundColor: AppColors.offWhiteBlue,
-    brightness: Brightness.light,
-    colorScheme: ColorScheme.fromSeed(
-      seedColor: Colors.deepPurple,
-      primary: AppColors.blue,
-      secondary: AppColors.black,
-      tertiary: AppColors.gray,
-      onTertiary: AppColors.red,
-      onPrimaryContainer: AppColors.offWhiteBlue,
-    ),
-    textTheme: TextTheme(
-      displaySmall: GoogleFonts.jockeyOne(
-        fontSize: 36,
-        fontWeight: FontWeight.w400,
-        color: AppColors.blue,
+      useMaterial3: true,
+      scaffoldBackgroundColor: AppColors.offWhiteBlue,
+      colorScheme: ColorScheme.fromSeed(
+        seedColor: Colors.deepPurple,
+        primary: AppColors.blue,
+        onPrimary: AppColors.blue,
+        secondary: AppColors.black,
+        tertiary: AppColors.gray,
+        onTertiary: AppColors.red,
+        onPrimaryContainer: AppColors.offWhiteBlue,
+        onSurface: AppColors.white,
+        brightness: Brightness.light,
       ),
-      titleLarge: GoogleFonts.inter(
-        fontSize: 20,
-        fontWeight: FontWeight.w700,
-        color: AppColors.blue,
-      ),
-    ),
-  );
-
-  static ThemeData darkTheme = ThemeData(
-    useMaterial3: true,
-    scaffoldBackgroundColor: AppColors.overDarkBlue,
-    brightness: Brightness.dark,
-    colorScheme: ColorScheme.fromSeed(
-      seedColor: Colors.deepPurple,
-      primary: AppColors.blue,
-      secondary: AppColors.whiteGray,
-      tertiary: AppColors.whiteGray,
-      onTertiary: AppColors.red,
-    ),
-    textTheme: TextTheme(
+      textTheme: TextTheme(
         displaySmall: GoogleFonts.jockeyOne(
           fontSize: 36,
           fontWeight: FontWeight.w400,
@@ -52,6 +27,48 @@ abstract class AppTheme {
           fontSize: 20,
           fontWeight: FontWeight.w700,
           color: AppColors.blue,
-        )),
-  );
+        ),
+        titleMedium: GoogleFonts.inter(
+          fontSize: 16,
+          fontWeight: FontWeight.w500,
+          color: AppColors.black,
+        ),
+      ),
+      appBarTheme: const AppBarTheme(
+        backgroundColor: AppColors.offWhiteBlue,
+      ));
+
+  static ThemeData darkTheme = ThemeData(
+      useMaterial3: true,
+      scaffoldBackgroundColor: AppColors.overDarkBlue,
+      colorScheme: ColorScheme.fromSeed(
+        seedColor: Colors.deepPurple,
+        primary: AppColors.blue,
+        onPrimary: AppColors.whiteGray,
+        secondary: AppColors.whiteGray,
+        tertiary: AppColors.whiteGray,
+        onTertiary: AppColors.red,
+        onSurface: AppColors.white,
+        brightness: Brightness.dark,
+      ),
+      textTheme: TextTheme(
+        displaySmall: GoogleFonts.jockeyOne(
+          fontSize: 36,
+          fontWeight: FontWeight.w400,
+          color: AppColors.blue,
+        ),
+        titleLarge: GoogleFonts.inter(
+          fontSize: 20,
+          fontWeight: FontWeight.w700,
+          color: AppColors.blue,
+        ),
+        titleMedium: GoogleFonts.inter(
+          fontSize: 16,
+          fontWeight: FontWeight.w500,
+          color: AppColors.whiteGray,
+        ),
+      ),
+      appBarTheme: const AppBarTheme(
+        backgroundColor: AppColors.overDarkBlue,
+      ));
 }
