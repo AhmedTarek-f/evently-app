5a985a64ddfbabc847ed924dffd3a7de0bebd538
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-04-30 19:42:13 +0300

    adding theme and lang functionalities

--- lib/evently_app.dart
+++ lib/evently_app.dart
@@ -1,5 +1,10 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:evently_app/core/utils/app_theme.dart';
+import 'package:evently_app/features/start/presentation/views/start_view.dart';
+import 'package:evently_app/features/start/presentation/views_model/start_cubit.dart';
+import 'package:evently_app/features/start/presentation/views_model/start_state.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class EventlyApp extends StatelessWidget {
@@ -7,15 +12,24 @@ class EventlyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final controller = BlocProvider.of<StartCubit>(context);
     return ScreenUtilInit(
       designSize: const Size(393, 841),
-      child: MaterialApp(
-        title: 'Evently',
-        debugShowCheckedModeBanner: false,
-        themeMode: AppTheme.appTheme,
-        theme: AppTheme.lightTheme,
-        darkTheme: AppTheme.darkTheme,
-        home: const Placeholder(),
+      child: BlocBuilder<StartCubit, StartState>(
+        buildWhen: (previous, current) =>
+            current is ChangeLanguageIndexState ||
+            current is ChangeThemeIndexState,
+        builder: (context, state) => MaterialApp(
+          title: 'Evently',
+          localizationsDelegates: context.localizationDelegates,
+          supportedLocales: context.supportedLocales,
+          locale: context.locale,
+          debugShowCheckedModeBanner: false,
+          themeMode: controller.isDarkMode ? ThemeMode.dark : ThemeMode.light,
+          theme: AppTheme.lightTheme,
+          darkTheme: AppTheme.darkTheme,
+          home: const StartView(),
+        ),
       ),
     );
   }
