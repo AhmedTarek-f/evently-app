7d86244346d3976871e26391e2e5a7e016376116
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-05-08 21:52:03 +0300

    adding login button

---
+++ lib/features/auth/login/presentation/views/widgets/login_button.dart
@@ -0,0 +1,15 @@
+import 'package:evently_app/core/common_widgets/custom_elevated_button.dart';
+import 'package:evently_app/core/constants/app_text.dart';
+import 'package:flutter/material.dart';
+
+class LoginButton extends StatelessWidget {
+  const LoginButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomElevatedButton(
+      onPressed: () {},
+      buttonTitle: AppText.login,
+    );
+  }
+}
