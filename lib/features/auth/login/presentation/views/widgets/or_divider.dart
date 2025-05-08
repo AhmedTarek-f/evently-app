25b3736612f2e09785f2bef9598a0d4e9e4b3c18
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-05-08 21:51:29 +0300

    adding or divier

---
+++ lib/features/auth/login/presentation/views/widgets/or_divider.dart
@@ -0,0 +1,33 @@
+import 'package:evently_app/core/constants/app_text.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class OrDivider extends StatelessWidget {
+  const OrDivider({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 42),
+      child: Row(
+        children: [
+          Expanded(
+              child: Divider(color: Theme.of(context).colorScheme.primary)),
+          RPadding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Text(
+              AppText.or,
+              style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+            ),
+          ),
+          Expanded(
+              child: Divider(
+            color: Theme.of(context).colorScheme.primary,
+          )),
+        ],
+      ),
+    );
+  }
+}
