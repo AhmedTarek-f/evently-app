668bbaf8a7830f60e1a6d443a007e43651df383c
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-05-08 21:48:58 +0300

    adding navigation to login view

--- lib/features/onboarding/presentation/views/widgets/onboarding_view_body.dart
+++ lib/features/onboarding/presentation/views/widgets/onboarding_view_body.dart
@@ -1,6 +1,10 @@
+import 'package:evently_app/features/auth/login/presentation/views/login_view.dart';
 import 'package:evently_app/features/onboarding/presentation/views/widgets/onboarding_page_navigation_dots.dart';
 import 'package:evently_app/features/onboarding/presentation/views/widgets/onboarding_page_view.dart';
+import 'package:evently_app/features/onboarding/presentation/views_model/onboarding_cubit.dart';
+import 'package:evently_app/features/onboarding/presentation/views_model/onboarding_state.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class OnboardingViewBody extends StatelessWidget {
@@ -8,15 +12,26 @@ class OnboardingViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const RPadding(
-      padding: EdgeInsets.symmetric(horizontal: 18),
-      child: Column(
-        children: [
-          Expanded(
-            child: OnboardingPageView(),
-          ),
-          OnboardingPageNavigationDots(),
-        ],
+    return BlocListener<OnboardingCubit, OnboardingState>(
+      listenWhen: (previous, current) => current is MovingToLoginScreenState,
+      listener: (context, state) {
+        if (state is MovingToLoginScreenState) {
+          Navigator.pushNamed(
+            context,
+            LoginView.routeName,
+          );
+        }
+      },
+      child: const RPadding(
+        padding: EdgeInsets.symmetric(horizontal: 18),
+        child: Column(
+          children: [
+            Expanded(
+              child: OnboardingPageView(),
+            ),
+            OnboardingPageNavigationDots(),
+          ],
+        ),
       ),
     );
   }
