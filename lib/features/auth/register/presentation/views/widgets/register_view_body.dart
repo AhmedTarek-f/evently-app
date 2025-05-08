dbd6470b371bfa2e8b257b5fb11b090a67f71a3f
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-05-08 22:16:34 +0300

    adding register view body

---
+++ lib/features/auth/register/presentation/views/widgets/register_view_body.dart
@@ -0,0 +1,36 @@
+import 'package:evently_app/core/common_widgets/evently_logo_column.dart';
+import 'package:evently_app/core/common_widgets/language_switch_row.dart';
+import 'package:evently_app/features/auth/register/presentation/views/widgets/already_have_account_row.dart';
+import 'package:evently_app/features/auth/register/presentation/views/widgets/create_account_button.dart';
+import 'package:evently_app/features/auth/register/presentation/views/widgets/register_form.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class RegisterViewBody extends StatelessWidget {
+  const RegisterViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SingleChildScrollView(
+      child: RPadding(
+        padding: EdgeInsets.symmetric(
+          horizontal: 16,
+        ),
+        child: Column(
+          children: [
+            RSizedBox(height: 32),
+            EventlyLogoColumn(),
+            RSizedBox(height: 24),
+            RegisterForm(),
+            RSizedBox(height: 24),
+            CreateAccountButton(),
+            RSizedBox(height: 14),
+            AlreadyHaveAccountRow(),
+            RSizedBox(height: 22),
+            LanguageSwitchRow(isLanguageTextVisible: false),
+          ],
+        ),
+      ),
+    );
+  }
+}
