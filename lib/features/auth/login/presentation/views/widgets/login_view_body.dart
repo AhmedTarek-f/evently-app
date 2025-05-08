25ac86b2f0ba8eef2388d765692ef81b721a27d0
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-05-08 21:51:46 +0300

    adding login view body

---
+++ lib/features/auth/login/presentation/views/widgets/login_view_body.dart
@@ -0,0 +1,45 @@
+import 'package:evently_app/core/common_widgets/evently_logo_column.dart';
+import 'package:evently_app/core/common_widgets/language_switch_row.dart';
+import 'package:evently_app/features/auth/login/presentation/views/widgets/create_account_row.dart';
+import 'package:evently_app/features/auth/login/presentation/views/widgets/forget_password_button.dart';
+import 'package:evently_app/features/auth/login/presentation/views/widgets/google_button.dart';
+import 'package:evently_app/features/auth/login/presentation/views/widgets/login_button.dart';
+import 'package:evently_app/features/auth/login/presentation/views/widgets/login_form.dart';
+import 'package:evently_app/features/auth/login/presentation/views/widgets/or_divider.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class LoginViewBody extends StatelessWidget {
+  const LoginViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SingleChildScrollView(
+      child: RPadding(
+        padding: EdgeInsets.symmetric(
+          horizontal: 16,
+        ),
+        child: Column(
+          children: [
+            RSizedBox(height: 12),
+            EventlyLogoColumn(),
+            RSizedBox(height: 24),
+            LoginForm(),
+            RSizedBox(height: 14),
+            ForgetPasswordButton(),
+            RSizedBox(height: 22),
+            LoginButton(),
+            RSizedBox(height: 22),
+            CreateAccountRow(),
+            RSizedBox(height: 22),
+            OrDivider(),
+            RSizedBox(height: 24),
+            GoogleButton(),
+            RSizedBox(height: 24),
+            LanguageSwitchRow(isLanguageTextVisible: false),
+          ],
+        ),
+      ),
+    );
+  }
+}
