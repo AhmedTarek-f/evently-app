d14b1c0b7cb020802c04fdb38abead95c740faab
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-05-08 22:47:20 +0300

    adding black color 2

--- lib/core/constants/app_colors.dart
+++ lib/core/constants/app_colors.dart
@@ -5,6 +5,7 @@ abstract final class AppColors {
   static const Color overDarkBlue = Color(0xff101127);
   static const Color blue = Color(0xff5669FF);
   static const Color black = Color(0xff1C1C1C);
+  static const Color black2 = Color(0xff101127);
   static const Color white = Color(0xffFFFFFF);
   static const Color whiteGray = Color(0xffF4EBDC);
   static const Color gray = Color(0xff7B7B7B);
