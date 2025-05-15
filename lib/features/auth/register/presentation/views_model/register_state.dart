5c70f2125ffe429a00a72e15320aa77a78b8ce4d
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-05-15 12:41:37 +0300

    adding new register states

--- lib/features/auth/register/presentation/views_model/register_state.dart
+++ lib/features/auth/register/presentation/views_model/register_state.dart
@@ -1,3 +1,11 @@
 sealed class RegisterState {}
 
 final class RegisterInitial extends RegisterState {}
+
+final class EnableAutoValidateModeState extends RegisterState {}
+
+final class FormValidationSuccessState extends RegisterState {}
+
+final class ChangePasswordObscureState extends RegisterState {}
+
+final class ChangeConfirmPasswordObscureState extends RegisterState {}
