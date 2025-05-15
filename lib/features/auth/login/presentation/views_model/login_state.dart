e59cc3d22fb6800f7dc1ed9f67fda5f82ba07005
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-05-15 12:08:24 +0300

    adding new login states

--- lib/features/auth/login/presentation/views_model/login_state.dart
+++ lib/features/auth/login/presentation/views_model/login_state.dart
@@ -1,3 +1,9 @@
 sealed class LoginState {}
 
 final class LoginInitial extends LoginState {}
+
+final class EnableAutoValidateModeState extends LoginState {}
+
+final class ChangeObscureState extends LoginState {}
+
+final class FormValidationSuccessState extends LoginState {}
