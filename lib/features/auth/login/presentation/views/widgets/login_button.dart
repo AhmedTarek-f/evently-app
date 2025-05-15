861332ac390b5e250e6b7508a530f1d8418b0d72
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-05-15 12:09:08 +0300

    using login function

--- lib/features/auth/login/presentation/views/widgets/login_button.dart
+++ lib/features/auth/login/presentation/views/widgets/login_button.dart
@@ -1,14 +1,19 @@
 import 'package:evently_app/core/common_widgets/custom_elevated_button.dart';
 import 'package:evently_app/core/constants/app_text.dart';
+import 'package:evently_app/features/auth/login/presentation/views_model/login_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class LoginButton extends StatelessWidget {
   const LoginButton({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final controller = BlocProvider.of<LoginCubit>(context);
     return CustomElevatedButton(
-      onPressed: () {},
+      onPressed: () {
+        controller.login();
+      },
       buttonTitle: AppText.login,
     );
   }
