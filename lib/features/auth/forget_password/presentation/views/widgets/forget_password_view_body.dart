6dc39a976b9427b1854c246e2f1d3ca53ecdfcab
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-05-15 13:08:23 +0300

    adding bloc listener

--- lib/features/auth/forget_password/presentation/views/widgets/forget_password_view_body.dart
+++ lib/features/auth/forget_password/presentation/views/widgets/forget_password_view_body.dart
@@ -1,6 +1,10 @@
 import 'package:evently_app/core/constants/app_images.dart';
+import 'package:evently_app/features/auth/forget_password/presentation/views/widgets/forget_password_email_field.dart';
 import 'package:evently_app/features/auth/forget_password/presentation/views/widgets/reset_password_button.dart';
+import 'package:evently_app/features/auth/forget_password/presentation/views_model/forget_password_cubit.dart';
+import 'package:evently_app/features/auth/forget_password/presentation/views_model/forget_password_state.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class ForgetPasswordViewBody extends StatelessWidget {
@@ -8,18 +12,29 @@ class ForgetPasswordViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return RPadding(
-      padding: const EdgeInsets.symmetric(horizontal: 16),
-      child: Column(
-        children: [
-          Image.asset(
-            AppImages.forgetPassword,
-            width: ScreenUtil().screenWidth,
-            fit: BoxFit.contain,
+    return BlocListener<ForgetPasswordCubit, ForgetPasswordState>(
+      listenWhen: (previous, current) => current is FormValidationSuccessState,
+      listener: (context, state) {
+        if (state is FormValidationSuccessState) {}
+      },
+      child: SingleChildScrollView(
+        physics: const BouncingScrollPhysics(),
+        child: RPadding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Column(
+            children: [
+              Image.asset(
+                AppImages.forgetPassword,
+                width: ScreenUtil().screenWidth,
+                fit: BoxFit.contain,
+              ),
+              const RSizedBox(height: 24),
+              const ForgetPasswordEmailField(),
+              const RSizedBox(height: 16),
+              const ResetPasswordButton(),
+            ],
           ),
-          const RSizedBox(height: 24),
-          const ResetPasswordButton(),
-        ],
+        ),
       ),
     );
   }
