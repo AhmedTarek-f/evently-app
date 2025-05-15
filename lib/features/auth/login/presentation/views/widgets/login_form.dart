4ff9ac74941a7896ecfbf7d56cfc2b23a7ab3177
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-05-15 12:08:50 +0300

    adding login form logic

--- lib/features/auth/login/presentation/views/widgets/login_form.dart
+++ lib/features/auth/login/presentation/views/widgets/login_form.dart
@@ -1,7 +1,11 @@
 import 'package:evently_app/core/common_widgets/custom_text_form_field.dart';
 import 'package:evently_app/core/constants/app_icons.dart';
 import 'package:evently_app/core/constants/app_text.dart';
+import 'package:evently_app/core/utils/validations.dart';
+import 'package:evently_app/features/auth/login/presentation/views_model/login_cubit.dart';
+import 'package:evently_app/features/auth/login/presentation/views_model/login_state.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
@@ -10,50 +14,70 @@ class LoginForm extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Form(
-      child: Column(
-        children: [
-          CustomTextFormField(
-            labelText: AppText.email,
-            textInputAction: TextInputAction.next,
-            prefixIcon: RPadding(
-              padding: const EdgeInsets.symmetric(horizontal: 16),
-              child: SvgPicture.asset(
-                AppIcons.email,
-                width: 56.r,
-                height: 56.r,
-                colorFilter: ColorFilter.mode(
-                  Theme.of(context).colorScheme.tertiary,
-                  BlendMode.srcIn,
+    final controller = BlocProvider.of<LoginCubit>(context);
+    return BlocBuilder<LoginCubit, LoginState>(
+      buildWhen: (previous, current) =>
+          current is EnableAutoValidateModeState ||
+          current is ChangeObscureState,
+      builder: (context, state) => Form(
+        key: controller.loginFormKey,
+        autovalidateMode: controller.autoValidateMode,
+        child: Column(
+          children: [
+            CustomTextFormField(
+              labelText: AppText.email,
+              textInputAction: TextInputAction.next,
+              keyboardType: TextInputType.emailAddress,
+              controller: controller.emailController,
+              prefixIcon: RPadding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: SvgPicture.asset(
+                  AppIcons.email,
+                  width: 56.r,
+                  height: 56.r,
+                  colorFilter: ColorFilter.mode(
+                    Theme.of(context).colorScheme.tertiary,
+                    BlendMode.srcIn,
+                  ),
                 ),
               ),
+              validator: (value) => Validations.emailValidation(value),
             ),
-          ),
-          const RSizedBox(height: 16),
-          CustomTextFormField(
-            labelText: AppText.password,
-            textInputAction: TextInputAction.done,
-            prefixIcon: RPadding(
-              padding: const EdgeInsets.symmetric(horizontal: 16),
-              child: SvgPicture.asset(
-                AppIcons.passwordLock,
-                width: 56.r,
-                height: 56.r,
-                colorFilter: ColorFilter.mode(
-                  Theme.of(context).colorScheme.tertiary,
-                  BlendMode.srcIn,
+            const RSizedBox(height: 16),
+            CustomTextFormField(
+              labelText: AppText.password,
+              controller: controller.passwordController,
+              textInputAction: TextInputAction.done,
+              obscureText: controller.isObscure,
+              keyboardType: TextInputType.visiblePassword,
+              prefixIcon: RPadding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: SvgPicture.asset(
+                  AppIcons.passwordLock,
+                  width: 56.r,
+                  height: 56.r,
+                  colorFilter: ColorFilter.mode(
+                    Theme.of(context).colorScheme.tertiary,
+                    BlendMode.srcIn,
+                  ),
                 ),
               ),
-            ),
-            suffixIcon: IconButton(
-                onPressed: () {},
+              suffixIcon: IconButton(
+                onPressed: () {
+                  controller.toggleObscure();
+                },
                 icon: Icon(
-                  Icons.visibility_off,
+                  controller.isObscure
+                      ? Icons.visibility_off
+                      : Icons.visibility,
                   size: 20,
                   color: Theme.of(context).colorScheme.tertiary,
-                )),
-          ),
-        ],
+                ),
+              ),
+              validator: (value) => Validations.validatePassword(value),
+            ),
+          ],
+        ),
       ),
     );
   }
