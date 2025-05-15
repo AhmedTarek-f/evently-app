6ec3f4c49ce7b0dbdd92b9158be1b11b5508b6f8
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-05-15 12:08:09 +0300

    adding login cubit logic

--- lib/features/auth/login/presentation/views_model/login_cubit.dart
+++ lib/features/auth/login/presentation/views_model/login_cubit.dart
@@ -1,6 +1,46 @@
 import 'package:evently_app/features/auth/login/presentation/views_model/login_state.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class LoginCubit extends Cubit<LoginState> {
-  LoginCubit() : super(LoginInitial());
+  LoginCubit() : super(LoginInitial()) {
+    onInit();
+  }
+
+  late final GlobalKey<FormState> loginFormKey;
+  late final TextEditingController emailController;
+  late final TextEditingController passwordController;
+  bool isObscure = true;
+  AutovalidateMode autoValidateMode = AutovalidateMode.disabled;
+
+  void onInit() {
+    loginFormKey = GlobalKey<FormState>();
+    emailController = TextEditingController();
+    passwordController = TextEditingController();
+  }
+
+  void enableAutoValidateMode() {
+    autoValidateMode = AutovalidateMode.always;
+    emit(EnableAutoValidateModeState());
+  }
+
+  void toggleObscure() {
+    isObscure = !isObscure;
+    emit(ChangeObscureState());
+  }
+
+  void login() {
+    if (loginFormKey.currentState!.validate()) {
+      emit(FormValidationSuccessState());
+    } else {
+      enableAutoValidateMode();
+    }
+  }
+
+  @override
+  Future<void> close() {
+    emailController.dispose();
+    passwordController.dispose();
+    return super.close();
+  }
 }
