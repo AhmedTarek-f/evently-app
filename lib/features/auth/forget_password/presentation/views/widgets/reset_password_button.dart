82e2e0e560e622417586d9dcd1665ddf909557a1
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-05-15 13:08:40 +0300

    using reset password function

--- lib/features/auth/forget_password/presentation/views/widgets/reset_password_button.dart
+++ lib/features/auth/forget_password/presentation/views/widgets/reset_password_button.dart
@@ -1,14 +1,19 @@
 import 'package:evently_app/core/common_widgets/custom_elevated_button.dart';
 import 'package:evently_app/core/constants/app_text.dart';
+import 'package:evently_app/features/auth/forget_password/presentation/views_model/forget_password_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class ResetPasswordButton extends StatelessWidget {
   const ResetPasswordButton({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final controller = BlocProvider.of<ForgetPasswordCubit>(context);
     return CustomElevatedButton(
-      onPressed: () {},
+      onPressed: () {
+        controller.resetPassword();
+      },
       buttonTitle: AppText.resetPassword,
     );
   }
