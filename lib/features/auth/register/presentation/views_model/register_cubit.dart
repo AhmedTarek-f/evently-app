0d3670a41ce1c1ceada69b15e20faa4baf6152b9
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-05-15 12:41:14 +0300

    adding register cubit logic

--- lib/features/auth/register/presentation/views_model/register_cubit.dart
+++ lib/features/auth/register/presentation/views_model/register_cubit.dart
@@ -1,6 +1,58 @@
 import 'package:evently_app/features/auth/register/presentation/views_model/register_state.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class RegisterCubit extends Cubit<RegisterState> {
-  RegisterCubit() : super(RegisterInitial());
+  RegisterCubit() : super(RegisterInitial()) {
+    onInit();
+  }
+
+  late final GlobalKey<FormState> createAccountFormKey;
+  late final TextEditingController userNameController;
+  late final TextEditingController emailController;
+  late final TextEditingController passwordController;
+  late final TextEditingController confirmPasswordController;
+  bool isPasswordObscure = true;
+  bool isConfirmPasswordObscure = true;
+  AutovalidateMode autoValidateMode = AutovalidateMode.disabled;
+
+  void onInit() {
+    createAccountFormKey = GlobalKey<FormState>();
+    userNameController = TextEditingController();
+    emailController = TextEditingController();
+    passwordController = TextEditingController();
+    confirmPasswordController = TextEditingController();
+  }
+
+  void enableAutoValidateMode() {
+    autoValidateMode = AutovalidateMode.always;
+    emit(EnableAutoValidateModeState());
+  }
+
+  void togglePasswordObscure() {
+    isPasswordObscure = !isPasswordObscure;
+    emit(ChangePasswordObscureState());
+  }
+
+  void toggleConfirmPasswordObscure() {
+    isConfirmPasswordObscure = !isConfirmPasswordObscure;
+    emit(ChangeConfirmPasswordObscureState());
+  }
+
+  void createAccount() {
+    if (createAccountFormKey.currentState!.validate()) {
+      emit(FormValidationSuccessState());
+    } else {
+      enableAutoValidateMode();
+    }
+  }
+
+  @override
+  Future<void> close() {
+    userNameController.dispose();
+    emailController.dispose();
+    passwordController.dispose();
+    confirmPasswordController.dispose();
+    return super.close();
+  }
 }
