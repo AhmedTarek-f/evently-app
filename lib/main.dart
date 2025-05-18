112ff329db4d39c6effe43191828bbd1bd3ac847
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-05-18 14:37:20 +0300

    initialize firebase

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:evently_app/core/cache/shared_preferences_helper.dart';
 import 'package:evently_app/evently_app.dart';
 import 'package:evently_app/features/start/presentation/views_model/start_cubit.dart';
+import 'package:evently_app/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
@@ -9,6 +11,9 @@ import 'package:flutter_native_splash/flutter_native_splash.dart';
 void main() async {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await SharedPreferencesHelper.init();
   await EasyLocalization.ensureInitialized();
   FlutterNativeSplash.remove();
