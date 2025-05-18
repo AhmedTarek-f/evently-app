72f6e196b31a925109c64f1d566fe870da8c8de7
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-05-18 14:40:44 +0300

    adding sign in with google service

---
+++ lib/core/utils/services/auth_services/sign_in_with_google_service/sign_in_with_google_service.dart
@@ -0,0 +1,37 @@
+import 'package:dartz/dartz.dart';
+import 'package:evently_app/core/constants/app_text.dart';
+import 'package:evently_app/core/utils/errors/failure.dart';
+import 'package:evently_app/core/utils/errors/firebase_errors.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+abstract class SignInWithGoogleService {
+  static final _auth = FirebaseAuth.instance;
+  static Future<Either<Failure, UserCredential>> signInWithGoogle() async {
+    try {
+      final GoogleSignInAccount? userAccount = await GoogleSignIn().signIn();
+
+      final GoogleSignInAuthentication? googleAuth =
+          await userAccount?.authentication;
+
+      final credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth?.accessToken,
+        idToken: googleAuth?.idToken,
+      );
+      UserCredential userCredential =
+          await _auth.signInWithCredential(credential);
+      return right(userCredential);
+    } catch (e) {
+      if (e is FirebaseAuthException) {
+        return left(FirebaseErrors.firebaseAuth(e));
+      } else if (e is FirebaseException) {
+        return left(FirebaseErrors.firebaseExceptions(e));
+      }
+      return left(
+        FirebaseErrors(
+          errorMessage: "${AppText.unknownErrorMessage} ${e.toString()}",
+        ),
+      );
+    }
+  }
+}
