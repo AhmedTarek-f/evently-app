26acdc654f6bbbbbd15f375b8a20a7f37da467a7
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-05-18 20:19:30 +0300

    adding bottom navigation bar theme

--- lib/core/utils/app_theme.dart
+++ lib/core/utils/app_theme.dart
@@ -1,5 +1,6 @@
 import 'package:evently_app/core/constants/app_colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 abstract class AppTheme {
@@ -18,51 +19,52 @@ abstract class AppTheme {
       onSurface: AppColors.white,
       primaryFixed: AppColors.offWhiteBlue,
       surfaceContainer: AppColors.gray,
+      inversePrimary: AppColors.blue,
       brightness: Brightness.light,
     ),
     textTheme: TextTheme(
       displaySmall: GoogleFonts.jockeyOne(
-        fontSize: 36,
+        fontSize: 36.sp,
         fontWeight: FontWeight.w400,
         color: AppColors.blue,
       ),
       titleLarge: GoogleFonts.inter(
-        fontSize: 20,
+        fontSize: 20.sp,
         fontWeight: FontWeight.w700,
         color: AppColors.blue,
       ),
       titleMedium: GoogleFonts.inter(
-        fontSize: 16,
+        fontSize: 16.sp,
         fontWeight: FontWeight.w500,
         color: AppColors.black,
       ),
       bodyLarge: GoogleFonts.inter(
-        fontSize: 16,
+        fontSize: 16.sp,
         color: AppColors.gray,
         fontWeight: FontWeight.w500,
       ),
       bodyMedium: GoogleFonts.inter(
-        fontSize: 16,
+        fontSize: 16.sp,
         color: AppColors.black,
         fontWeight: FontWeight.w500,
       ),
       titleSmall: GoogleFonts.inter(
-        fontSize: 14,
+        fontSize: 14.sp,
         color: AppColors.red,
         fontWeight: FontWeight.w500,
       ),
       labelSmall: GoogleFonts.inter(
-        fontSize: 14,
+        fontSize: 14.sp,
         color: AppColors.gray.withValues(alpha: 0.6),
         fontWeight: FontWeight.w400,
       ),
       headlineSmall: GoogleFonts.inter(
-        fontSize: 22,
+        fontSize: 22.sp,
         color: AppColors.black2,
         fontWeight: FontWeight.w400,
       ),
       headlineMedium: GoogleFonts.inter(
-        fontSize: 24,
+        fontSize: 24.sp,
         color: AppColors.black,
         fontWeight: FontWeight.w400,
       ),
@@ -71,6 +73,20 @@ abstract class AppTheme {
       backgroundColor: AppColors.offWhiteBlue,
       centerTitle: true,
       scrolledUnderElevation: 0,
+    ),
+    bottomNavigationBarTheme: BottomNavigationBarThemeData(
+      type: BottomNavigationBarType.fixed,
+      backgroundColor: AppColors.blue,
+      showSelectedLabels: true,
+      showUnselectedLabels: true,
+      unselectedLabelStyle: GoogleFonts.inter(
+        fontSize: 12.sp,
+        fontWeight: FontWeight.w700,
+      ),
+      selectedLabelStyle: GoogleFonts.inter(
+        fontSize: 12.sp,
+        fontWeight: FontWeight.w700,
+      ),
     ),
   );
 
@@ -87,60 +103,81 @@ abstract class AppTheme {
       onTertiary: AppColors.red,
       onSurface: AppColors.white,
       primaryFixed: AppColors.overDarkBlue,
+      onPrimaryContainer: AppColors.whiteGray,
       surfaceContainer: AppColors.blue,
+      inversePrimary: AppColors.overDarkBlue,
       brightness: Brightness.dark,
     ),
     textTheme: TextTheme(
       displaySmall: GoogleFonts.jockeyOne(
-        fontSize: 36,
+        fontSize: 36.sp,
         fontWeight: FontWeight.w400,
         color: AppColors.blue,
       ),
       titleLarge: GoogleFonts.inter(
-        fontSize: 20,
+        fontSize: 20.sp,
         fontWeight: FontWeight.w700,
         color: AppColors.blue,
       ),
       titleMedium: GoogleFonts.inter(
-        fontSize: 16,
+        fontSize: 16.sp,
         fontWeight: FontWeight.w500,
         color: AppColors.whiteGray,
       ),
       bodyLarge: GoogleFonts.inter(
-        fontSize: 16,
+        fontSize: 16.sp,
         color: AppColors.whiteGray,
         fontWeight: FontWeight.w500,
       ),
       bodyMedium: GoogleFonts.inter(
-        fontSize: 16,
+        fontSize: 16.sp,
         color: AppColors.white,
         fontWeight: FontWeight.w500,
       ),
       titleSmall: GoogleFonts.inter(
-        fontSize: 14,
+        fontSize: 14.sp,
         color: AppColors.red,
         fontWeight: FontWeight.w500,
       ),
       labelSmall: GoogleFonts.inter(
-        fontSize: 14,
+        fontSize: 14.sp,
         color: AppColors.whiteGray.withValues(alpha: 0.6),
         fontWeight: FontWeight.w400,
       ),
       headlineSmall: GoogleFonts.inter(
-        fontSize: 22,
+        fontSize: 22.sp,
         color: AppColors.blue,
         fontWeight: FontWeight.w400,
       ),
       headlineMedium: GoogleFonts.inter(
-        fontSize: 24,
+        fontSize: 24.sp,
         color: AppColors.whiteGray,
         fontWeight: FontWeight.w400,
+      ),
+      bodySmall: GoogleFonts.inter(
+        fontSize: 12.sp,
+        color: AppColors.whiteGray,
+        fontWeight: FontWeight.w700,
       ),
     ),
     appBarTheme: const AppBarTheme(
       backgroundColor: AppColors.overDarkBlue,
       centerTitle: true,
       scrolledUnderElevation: 0,
+    ),
+    bottomNavigationBarTheme: BottomNavigationBarThemeData(
+      type: BottomNavigationBarType.fixed,
+      backgroundColor: AppColors.overDarkBlue,
+      showSelectedLabels: true,
+      showUnselectedLabels: true,
+      unselectedLabelStyle: GoogleFonts.inter(
+        fontSize: 12.sp,
+        fontWeight: FontWeight.w700,
+      ),
+      selectedLabelStyle: GoogleFonts.inter(
+        fontSize: 12.sp,
+        fontWeight: FontWeight.w700,
+      ),
     ),
   );
 }
