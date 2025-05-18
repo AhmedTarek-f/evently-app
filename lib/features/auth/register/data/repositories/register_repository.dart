8288ddb84d7a9f1ccbc8d7827c8a54fa2b14cf3b
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-05-18 14:43:58 +0300

    adding register repository

---
+++ lib/features/auth/register/data/repositories/register_repository.dart
@@ -0,0 +1,17 @@
+import 'package:dartz/dartz.dart';
+import 'package:evently_app/core/utils/errors/failure.dart';
+import 'package:evently_app/core/utils/services/auth_services/register_with_email_and_password_service/register_with_email_and_password_service.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+abstract class RegisterRepository {
+  static Future<Either<Failure, UserCredential>> registerWithEmailAndPassword(
+    String email,
+    String password,
+  ) async {
+    return await RegisterWithEmailAndPasswordService
+        .registerWithEmailAndPassword(
+      email,
+      password,
+    );
+  }
+}
