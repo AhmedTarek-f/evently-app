d5a4361cdf9621727f5e2b8a6beda590a496c775
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-05-18 19:59:53 +0300

    adding evently navigation tap icons

--- lib/core/constants/app_icons.dart
+++ lib/core/constants/app_icons.dart
@@ -15,4 +15,29 @@ abstract final class AppIcons {
   static const String passwordLock = "assets/icons/password_lock.svg";
   static const String user = "assets/icons/user.svg";
   static const String google = "assets/icons/google.svg";
+  static const String activeFavoritesDark =
+      "assets/icons/active_favorites_dark.svg";
+  static const String activeFavoritesLight =
+      "assets/icons/active_favorites_light.svg";
+  static const String activeHomeDark = "assets/icons/active_home_dark.svg";
+  static const String activeHomeLight = "assets/icons/active_home_light.svg";
+  static const String activeMapDark = "assets/icons/active_map_dark.svg";
+  static const String activeMapLight = "assets/icons/active_map_light.svg";
+  static const String activeProfileDark =
+      "assets/icons/active_profile_dark.svg";
+  static const String activeProfileLight =
+      "assets/icons/active_profile_light.svg";
+  static const String inactiveFavoritesDark =
+      "assets/icons/inactive_favorites_dark.svg";
+  static const String inactiveFavoritesLight =
+      "assets/icons/inactive_favorites_light.svg";
+  static const String inactiveHomeDark = "assets/icons/inactive_home_dark.svg";
+  static const String inactiveHomeLight =
+      "assets/icons/inactive_home_light.svg";
+  static const String inactiveMapDark = "assets/icons/inactive_map_dark.svg";
+  static const String inactiveMapLight = "assets/icons/inactive_map_light.svg";
+  static const String inactiveProfileDark =
+      "assets/icons/inactive_profile_dark.svg";
+  static const String inactiveProfileLight =
+      "assets/icons/inactive_profile_light.svg";
 }
