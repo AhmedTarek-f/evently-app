ed78c66e21b956b3d4f881a110ac6469335a0dc0
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-05-18 14:49:15 +0300

    adding firebase auth text

--- lib/core/constants/app_text.dart
+++ lib/core/constants/app_text.dart
@@ -43,4 +43,116 @@ abstract final class AppText {
   static const String passwordConfirmationValidation2 =
       "Password cannot contain spaces";
   static const String passwordConfirmationValidation3 = "Passwords don't match";
+  static const String emailSent = "Email Sent";
+  static const String emailSentMessage =
+      "Please check your inbox and verify your email.";
+  static const String verifyYourEmailAddress = "Verify your email address!";
+  static const String verifyEmailCongratulations =
+      "Congratulations! Your Account Awaits: Verify Your Email Now.";
+  static const String continueText = "Continue";
+  static const String resendEmail = "Resend Email";
+  static const String yourAccountSuccessfullyCreated =
+      "Your account successfully created!";
+  static const String yourAccountSuccessfullyCreatedMessage =
+      "Welcome to Your Ultimate Events Destination Your Account is Created";
+  static const String passwordResetMessage =
+      "A password reset has been sent to your email";
+  static const String loggingYouIn = "Logging you in...";
+  static const String processingYourInformation =
+      "We are processing your information...";
+
+  // Firebase Exceptions
+  static const String emailAlreadyInUse =
+      'The email address is already registered. Please use a different email.';
+  static const String invalidEmail =
+      'The email address provided is invalid. Please enter a valid email.';
+  static const String networkRequestFailed =
+      'There is a network problem, Please check your network connection and try again.';
+  static const String weakPassword =
+      'The password is too weak. Please choose a stronger password.';
+  static const String userDisabled =
+      'This user account has been disabled. Please contact support for assistance.';
+  static const String userNotFound = 'Invalid login details. User not found.';
+  static const String invalidVerificationCode =
+      'Invalid verification code. Please enter a valid code.';
+  static const String invalidVerificationId =
+      'Invalid verification ID. Please request a new verification code.';
+  static const String quotaExceeded = 'Quota exceeded. Please try again later.';
+  static const String emailAlreadyExists =
+      'The email address already exists. Please use a different email.';
+  static const String providerAlreadyLinked =
+      'The account is already linked with another provider.';
+  static const String requiresRecentLogin =
+      'This operation is sensitive and requires recent authentication. Please log in again.';
+  static const String credentialAlreadyInUse =
+      'This credential is already associated with a different user account.';
+  static const String userMismatch =
+      'The supplied credentials do not correspond to the previously signed in user.';
+  static const String accountExistsWithDifferentCredential =
+      'An account already exists with the same email but different sign-in credentials.';
+  static const String operationNotAllowed =
+      'This operation is not allowed. Contact support for assistance.';
+  static const String expiredActionCode =
+      'The action code has expired. Please request a new action code.';
+  static const String invalidActionCode =
+      'The action code is invalid. Please check the code and try again.';
+  static const String missingActionCode =
+      'The action code is missing. Please provide a valid action code.';
+  static const String userTokenExpired =
+      'The user\'s token has expired, and authentication is required. Please sign in again.';
+  static const String wrongPassword =
+      'The password is invalid. Please check your password and try again.';
+  static const String userTokenRevoked =
+      'The user\'s token has been revoked. Please sign in again.';
+  static const String invalidMessagePayload =
+      'The email template verification message payload is invalid.';
+  static const String invalidSender =
+      'The email template sender is invalid. Please verify the sender\'s email.';
+  static const String invalidRecipientEmail =
+      'The recipient email address is invalid. Please provide a valid recipient email.';
+  static const String missingIframeStart =
+      'The email template is missing the iframe start tag.';
+  static const String missingIframeEnd =
+      'The email template is missing the iframe end tag.';
+  static const String missingIframeSrc =
+      'The email template is missing the iframe src attribute.';
+  static const String authDomainConfigRequired =
+      'The authDomain configuration is required for the action code verification link.';
+  static const String missingAppCredential =
+      'The app credential is missing. Please provide valid app credentials.';
+  static const String invalidAppCredential =
+      'The app credential is invalid. Please provide a valid app credential.';
+  static const String sessionCookieExpired =
+      'The Firebase session cookie has expired. Please sign in again.';
+  static const String uidAlreadyExists =
+      'The provided user ID is already in use by another user.';
+  static const String invalidCordovaConfiguration =
+      'The provided Cordova configuration is invalid.';
+  static const String appDeleted =
+      'This instance of FirebaseApp has been deleted.';
+  static const String userTokenMismatch =
+      'The provided user\'s token has a mismatch with the authenticated user\'s user ID.';
+  static const String webStorageUnsupported =
+      'Web storage is not supported or is disabled.';
+  static const String invalidCredential =
+      'The supplied credential is invalid. Please check the credential and try again.';
+  static const String appNotAuthorized =
+      'The app is not authorized to use Firebase Authentication with the provided API key.';
+  static const String keychainError =
+      'A keychain error occurred. Please check the keychain and try again.';
+  static const String internalError =
+      'An internal authentication error occurred. Please try again later.';
+  static const String invalidLoginCredentials = 'Invalid login credentials.';
+  static const String unknownAuthError =
+      'An unexpected authentication error occurred. Please try again.';
+  static const String unknownFirebaseError =
+      'An unknown Firebase error occurred. Please try again.';
+  static const String invalidCustomToken =
+      'The custom token format is incorrect. Please check your custom token.';
+  static const String customTokenMismatch =
+      'The custom token corresponds to a different audience.';
+  static const String captchaCheckFailed =
+      'The reCAPTCHA response is invalid. Please try again.';
+  static const String unknownErrorMessage =
+      "An unknown error has occurred with the following message:";
 }
