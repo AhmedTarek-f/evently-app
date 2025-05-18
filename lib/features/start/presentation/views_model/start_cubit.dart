9f29a1af901170742fd7941e05b6079009de009c
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-05-18 20:18:55 +0300

    adding screen redirect method

--- lib/features/start/presentation/views_model/start_cubit.dart
+++ lib/features/start/presentation/views_model/start_cubit.dart
@@ -1,6 +1,11 @@
 import 'package:evently_app/core/cache/shared_preferences_helper.dart';
 import 'package:evently_app/core/constants/const_keys.dart';
+import 'package:evently_app/features/auth/login/presentation/views/login_view.dart';
+import 'package:evently_app/features/auth/verify_email/presentation/views/verify_email_view.dart';
+import 'package:evently_app/features/evently_bottom_navigation/presentation/views/evently_bottom_navigation_view.dart';
+import 'package:evently_app/features/start/presentation/views/start_view.dart';
 import 'package:evently_app/features/start/presentation/views_model/start_state.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class StartCubit extends Cubit<StartState> {
@@ -11,12 +16,17 @@ class StartCubit extends Cubit<StartState> {
   late int themeSelectedIndex;
   late bool isDarkMode;
   late bool isArLanguage;
+  late bool isHomeScreen;
+  late final String redirectedScreen;
+  final _user = FirebaseAuth.instance.currentUser;
 
   void onInit() {
     isDarkMode = SharedPreferencesHelper.getBool(key: ConstKeys.isDarkTheme);
     isArLanguage = SharedPreferencesHelper.getBool(key: ConstKeys.isArLanguage);
+    isHomeScreen = SharedPreferencesHelper.getBool(key: ConstKeys.isHomeScreen);
     themeSelectedIndex = isDarkMode ? 1 : 0;
     languageSelectedIndex = isArLanguage ? 1 : 0;
+    screenRedirect();
   }
 
   Future<void> onLanguageIndexChanged({required int index}) async {
@@ -48,6 +58,22 @@ class StartCubit extends Cubit<StartState> {
           key: ConstKeys.isDarkTheme, value: true);
       isDarkMode = true;
       emit(ChangeThemeIndexState());
+    }
+  }
+
+  void screenRedirect() {
+    if (isHomeScreen) {
+      if (_user != null) {
+        if (_user.emailVerified) {
+          redirectedScreen = EventlyBottomNavigationView.routeName;
+        } else {
+          redirectedScreen = VerifyEmailView.routeName;
+        }
+      } else {
+        redirectedScreen = LoginView.routeName;
+      }
+    } else {
+      redirectedScreen = StartView.routeName;
     }
   }
 }
