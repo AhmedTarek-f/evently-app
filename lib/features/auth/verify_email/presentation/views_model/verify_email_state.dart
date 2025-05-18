1cf4a804c2a363427aa3b4327e9d47b7b28b6308
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-05-18 14:43:32 +0300

    adding verify email state

---
+++ lib/features/auth/verify_email/presentation/views_model/verify_email_state.dart
@@ -0,0 +1,21 @@
+sealed class VerifyEmailState {}
+
+final class VerifyEmailInitial extends VerifyEmailState {}
+
+final class SendEmailVerificationSuccessState extends VerifyEmailState {}
+
+final class SendEmailVerificationLoadingState extends VerifyEmailState {}
+
+final class SendEmailVerificationFailureState extends VerifyEmailState {
+  SendEmailVerificationFailureState({required this.errorMessage});
+  final String errorMessage;
+}
+
+final class LogoutSuccessState extends VerifyEmailState {}
+
+final class EmailVerifiedState extends VerifyEmailState {}
+
+final class LogoutFailureState extends VerifyEmailState {
+  LogoutFailureState({required this.errorMessage});
+  final String errorMessage;
+}
