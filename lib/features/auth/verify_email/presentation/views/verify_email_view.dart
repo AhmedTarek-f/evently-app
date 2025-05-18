a9f40d6da77e351234ec7f43e5492b0773cf2b34
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-05-18 14:42:55 +0300

    adding verify email view

---
+++ lib/features/auth/verify_email/presentation/views/verify_email_view.dart
@@ -0,0 +1,50 @@
+import 'package:evently_app/core/utils/loaders/loaders.dart';
+import 'package:evently_app/features/auth/login/presentation/views/login_view.dart';
+import 'package:evently_app/features/auth/login/presentation/views_model/login_state.dart';
+import 'package:evently_app/features/auth/verify_email/presentation/views/widgets/verify_email_view_body.dart';
+import 'package:evently_app/features/auth/verify_email/presentation/views_model/verify_email_cubit.dart';
+import 'package:evently_app/features/auth/verify_email/presentation/views_model/verify_email_state.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class VerifyEmailView extends StatelessWidget {
+  static const String routeName = "VerifyEmailView";
+  const VerifyEmailView({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final controller = BlocProvider.of<VerifyEmailCubit>(context);
+    return BlocListener<VerifyEmailCubit, VerifyEmailState>(
+      listenWhen: (previous, current) =>
+          current is LogoutSuccessState || current is LoginFailureState,
+      listener: (context, state) {
+        if (state is LogoutFailureState) {
+          Loaders.showErrorMessage(
+            message: state.errorMessage,
+            context: context,
+          );
+        } else if (state is LoginSuccessState) {
+          Navigator.of(context).pushNamedAndRemoveUntil(
+            LoginView.routeName,
+            (route) => false,
+          );
+        }
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          actions: [
+            IconButton(
+              onPressed: () async => await controller.logout(),
+              icon: const Icon(CupertinoIcons.clear),
+            ),
+          ],
+        ),
+        body: const VerifyEmailViewBody(),
+      ),
+    );
+  }
+}
