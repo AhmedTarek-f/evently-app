606b0477ba31145ca3b6ea69b54db522556ec940
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-05-19 15:40:53 +0300

    adding logout button

---
+++ lib/features/profile/presentation/views/widgets/logout_button.dart
@@ -0,0 +1,52 @@
+import 'package:evently_app/core/common_widgets/custom_elevated_button.dart';
+import 'package:evently_app/core/constants/app_colors.dart';
+import 'package:evently_app/core/constants/app_icons.dart';
+import 'package:evently_app/features/profile/presentation/views_model/profile_cubit.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class LogoutButton extends StatelessWidget {
+  const LogoutButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final controller = BlocProvider.of<ProfileCubit>(context);
+    return RPadding(
+      padding: const EdgeInsets.only(
+        right: 16,
+        left: 16,
+        bottom: 32,
+      ),
+      child: CustomElevatedButton(
+        isText: false,
+        onPressed: () async {
+          await controller.logout();
+        },
+        backgroundColor: AppColors.red,
+        buttonTitle: "",
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            SvgPicture.asset(
+              AppIcons.logout,
+              width: 24.r,
+              height: 24.r,
+            ),
+            const RSizedBox(width: 8),
+            Text(
+              "Logout",
+              style: GoogleFonts.inter(
+                fontSize: 20.sp,
+                fontWeight: FontWeight.w400,
+                color: AppColors.white,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
