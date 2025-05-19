cbc7ee0a78e416e4c94b7016d4727ec0f4b514fc
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-05-19 15:47:21 +0300

    using logout success state

--- lib/features/auth/verify_email/presentation/views/verify_email_view.dart
+++ lib/features/auth/verify_email/presentation/views/verify_email_view.dart
@@ -1,6 +1,5 @@
 import 'package:evently_app/core/utils/loaders/loaders.dart';
 import 'package:evently_app/features/auth/login/presentation/views/login_view.dart';
-import 'package:evently_app/features/auth/login/presentation/views_model/login_state.dart';
 import 'package:evently_app/features/auth/verify_email/presentation/views/widgets/verify_email_view_body.dart';
 import 'package:evently_app/features/auth/verify_email/presentation/views_model/verify_email_cubit.dart';
 import 'package:evently_app/features/auth/verify_email/presentation/views_model/verify_email_state.dart';
@@ -19,14 +18,14 @@ class VerifyEmailView extends StatelessWidget {
     final controller = BlocProvider.of<VerifyEmailCubit>(context);
     return BlocListener<VerifyEmailCubit, VerifyEmailState>(
       listenWhen: (previous, current) =>
-          current is LogoutSuccessState || current is LoginFailureState,
+          current is LogoutSuccessState || current is LogoutSuccessState,
       listener: (context, state) {
         if (state is LogoutFailureState) {
           Loaders.showErrorMessage(
             message: state.errorMessage,
             context: context,
           );
-        } else if (state is LoginSuccessState) {
+        } else if (state is LogoutSuccessState) {
           Navigator.of(context).pushNamedAndRemoveUntil(
             LoginView.routeName,
             (route) => false,
