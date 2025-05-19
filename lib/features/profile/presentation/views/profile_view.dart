76f47b260fef0d74c31b7d989d92bed26d8bc35f
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-05-19 15:39:49 +0300

    adding profile view

--- lib/features/profile/presentation/views/profile_view.dart
+++ lib/features/profile/presentation/views/profile_view.dart
@@ -1,15 +1,16 @@
+import 'package:evently_app/features/profile/presentation/views/widgets/profile_view_body.dart';
+import 'package:evently_app/features/profile/presentation/views_model/profile_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class ProfileView extends StatelessWidget {
   const ProfileView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        "Profile Screen",
-        style: Theme.of(context).textTheme.displaySmall,
-      ),
+    return BlocProvider<ProfileCubit>(
+      create: (context) => ProfileCubit(),
+      child: const ProfileViewBody(),
     );
   }
 }
