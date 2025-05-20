c060c5193169ec3a5555dcc4c3f33cbfafa48f87
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-05-20 18:22:43 +0300

    adding event model

---
+++ lib/features/create_event/data/models/event_model.dart
@@ -0,0 +1,18 @@
+class EventModel {
+  EventModel({
+    this.id,
+    this.eventCategory,
+    this.eventTitle,
+    this.eventDescription,
+    this.eventDate,
+    this.eventTime,
+    this.eventLocation,
+  });
+  String? id;
+  String? eventCategory;
+  String? eventTitle;
+  String? eventDescription;
+  String? eventDate;
+  String? eventTime;
+  String? eventLocation;
+}
