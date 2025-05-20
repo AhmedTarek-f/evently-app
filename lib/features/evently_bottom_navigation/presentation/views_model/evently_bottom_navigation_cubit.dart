9f0a972b8c9c80d5d5e03e544e702dc1fd5bbe5a
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-05-20 12:29:10 +0300

    adding fetch user data method

--- lib/features/evently_bottom_navigation/presentation/views_model/evently_bottom_navigation_cubit.dart
+++ lib/features/evently_bottom_navigation/presentation/views_model/evently_bottom_navigation_cubit.dart
@@ -1,3 +1,5 @@
+import 'package:evently_app/core/utils/services/firestore_services/firestore_services.dart';
+import 'package:evently_app/features/evently_bottom_navigation/data/repositories/evently_bottom_nav_repository.dart';
 import 'package:evently_app/features/evently_bottom_navigation/presentation/views_model/evently_bottom_navigation_state.dart';
 import 'package:evently_app/features/favorite/presentation/views/favorite_view.dart';
 import 'package:evently_app/features/home/presentation/views/home_view.dart';
@@ -8,7 +10,9 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 class EventlyBottomNavigationCubit extends Cubit<EventlyBottomNavigationState> {
   EventlyBottomNavigationCubit()
-      : super(const EventlyBottomNavigationInitial(tapIndex: 0));
+      : super(const EventlyBottomNavigationInitial(tapIndex: 0)) {
+    fetchUserData();
+  }
 
   final List<Widget> eventlyNavigationList = const [
     HomeView(),
@@ -19,5 +23,22 @@ class EventlyBottomNavigationCubit extends Cubit<EventlyBottomNavigationState> {
 
   void changeIndex({required int index}) {
     emit(ChangeTapState(tapIndex: index));
+  }
+
+  Future<void> fetchUserData() async {
+    if (FireStoreServices.currentUserData == null) {
+      emit(const FetchUserDataLoadingState(tapIndex: 0));
+      var userDataResult = await EventlyBottomNavRepository.getUserData();
+      userDataResult.fold(
+        (failure) => emit(FetchUserDataFailureState(
+          tapIndex: 0,
+          errorMessage: failure.errorMessage,
+        )),
+        (userData) {
+          FireStoreServices.currentUserData = userData;
+          emit(const FetchUserDataSuccessState(tapIndex: 0));
+        },
+      );
+    }
   }
 }
