0f27da6dae11ec302010bcd6f86ceb1061a93b4e
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-05-20 18:29:14 +0300

    adding event card title and favorite row

---
+++ lib/core/common_widgets/event_card/event_card_title_and_favorite_row.dart
@@ -0,0 +1,50 @@
+import 'package:evently_app/core/constants/app_icons.dart';
+import 'package:evently_app/features/create_event/data/models/event_model.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class EventCardTitleAndFavoriteRow extends StatelessWidget {
+  const EventCardTitleAndFavoriteRow({
+    super.key,
+    required this.eventData,
+  });
+
+  final EventModel eventData;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: REdgeInsets.all(4),
+      decoration: BoxDecoration(
+        color: Theme.of(context).colorScheme.primaryFixed,
+        borderRadius: BorderRadius.circular(
+          8.r,
+        ),
+      ),
+      child: Row(
+        children: [
+          Expanded(
+            child: Text(
+              eventData.eventTitle ?? "",
+              style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                    fontSize: 14.sp,
+                    fontWeight: FontWeight.w700,
+                  ),
+              overflow: TextOverflow.ellipsis,
+              maxLines: 3,
+              textAlign: TextAlign.start,
+            ),
+          ),
+          const RSizedBox(width: 4),
+          IconButton(
+            onPressed: () {},
+            icon: SvgPicture.asset(
+              AppIcons.favoriteFilled,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
