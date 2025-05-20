c7676f7792e87b063ea442444e0a2ab0905b3286
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-05-20 11:59:27 +0300

    adding get user data method

--- lib/features/auth/login/data/repositories/login_repository.dart
+++ lib/features/auth/login/data/repositories/login_repository.dart
@@ -2,6 +2,8 @@ import 'package:dartz/dartz.dart';
 import 'package:evently_app/core/utils/errors/failure.dart';
 import 'package:evently_app/core/utils/services/auth_services/sign_in_with_email_and_password_service/sign_in_with_email_and_password_service.dart';
 import 'package:evently_app/core/utils/services/auth_services/sign_in_with_google_service/sign_in_with_google_service.dart';
+import 'package:evently_app/core/utils/services/firestore_services/firestore_services.dart';
+import 'package:evently_app/features/auth/register/data/models/user_model.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 abstract class LoginRepository {
@@ -17,5 +19,10 @@ abstract class LoginRepository {
 
   static Future<Either<Failure, UserCredential>> signInWithGoogle() async {
     return await SignInWithGoogleService.signInWithGoogle();
+  }
+
+  static Future<Either<Failure, UserModel>> getUserData() async {
+    return await FireStoreServices.getUser(
+        userId: FirebaseAuth.instance.currentUser?.uid ?? "");
   }
 }
