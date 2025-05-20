0680394da93ac95aebb91313e2dc945351371ad2
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-05-20 12:29:25 +0300

    adding fetch user data states

--- lib/features/evently_bottom_navigation/presentation/views_model/evently_bottom_navigation_state.dart
+++ lib/features/evently_bottom_navigation/presentation/views_model/evently_bottom_navigation_state.dart
@@ -19,3 +19,27 @@ final class ChangeTapState extends EventlyBottomNavigationState {
   @override
   List<Object?> get props => [super.tapIndex];
 }
+
+final class FetchUserDataSuccessState extends EventlyBottomNavigationState {
+  const FetchUserDataSuccessState({required super.tapIndex});
+
+  @override
+  List<Object?> get props => [super.tapIndex];
+}
+
+final class FetchUserDataLoadingState extends EventlyBottomNavigationState {
+  const FetchUserDataLoadingState({required super.tapIndex});
+
+  @override
+  List<Object?> get props => [super.tapIndex];
+}
+
+final class FetchUserDataFailureState extends EventlyBottomNavigationState {
+  const FetchUserDataFailureState({
+    required super.tapIndex,
+    required this.errorMessage,
+  });
+  final String errorMessage;
+  @override
+  List<Object?> get props => [super.tapIndex];
+}
