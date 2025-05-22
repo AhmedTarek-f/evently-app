d6c3121ebb3837ed1c701bc6322611fb46b98a12
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-05-22 22:50:29 +0300

    adding create event remote data

---
+++ lib/features/create_event/data/data_sources/remote_data_sources/create_event_remote_data.dart
@@ -0,0 +1,45 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:dartz/dartz.dart';
+import 'package:evently_app/core/constants/app_text.dart';
+import 'package:evently_app/core/utils/errors/failure.dart';
+import 'package:evently_app/core/utils/errors/firebase_errors.dart';
+import 'package:evently_app/features/create_event/data/models/event_model.dart';
+
+abstract class CreateEventRemoteData {
+  static final _db = FirebaseFirestore.instance;
+
+  static CollectionReference<EventModel> _getEventsCollection() {
+    return _db.collection("Events").withConverter(
+          fromFirestore: (snapshot, options) =>
+              EventModel.fromFireStore(snapshot),
+          toFirestore: (event, options) => event.toFireStore(),
+        );
+  }
+
+  static Future<Either<Failure, void>> createEvent({
+    required EventModel eventData,
+  }) async {
+    try {
+      final List<ConnectivityResult> connectivityResult =
+          await (Connectivity().checkConnectivity());
+
+      if (connectivityResult.contains(ConnectivityResult.none)) {
+        return left(const FirebaseErrors(
+          errorMessage: AppText.networkRequestFailed,
+        ));
+      }
+      await _getEventsCollection().doc(eventData.eventId).set(eventData);
+      return right(null);
+    } catch (error) {
+      if (error is FirebaseException) {
+        return left(FirebaseErrors.firebaseExceptions(error));
+      }
+      return left(
+        FirebaseErrors(
+          errorMessage: "${AppText.unknownErrorMessage} ${error.toString()}",
+        ),
+      );
+    }
+  }
+}
