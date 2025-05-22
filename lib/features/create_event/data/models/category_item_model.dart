9a7e354f7b21d153f1b03c3536c2ee4b4bea1a3d
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-05-22 22:50:48 +0300

    adding category item model

---
+++ lib/features/create_event/data/models/category_item_model.dart
@@ -0,0 +1,8 @@
+class CategoryItemModel {
+  const CategoryItemModel({
+    required this.categoryName,
+    required this.categoryIcon,
+  });
+  final String categoryName;
+  final String categoryIcon;
+}
