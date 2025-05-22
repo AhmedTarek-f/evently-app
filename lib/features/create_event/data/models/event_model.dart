e7c4f3993097a0118e0ea1105e2ddc2b2c7610b3
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-05-22 22:51:10 +0300

    adding to fire store and from fire store methods

--- lib/features/create_event/data/models/event_model.dart
+++ lib/features/create_event/data/models/event_model.dart
@@ -1,18 +1,60 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class EventModel {
   EventModel({
-    this.id,
+    this.eventCreatorId,
+    this.eventId,
     this.eventCategory,
     this.eventTitle,
     this.eventDescription,
     this.eventDate,
     this.eventTime,
-    this.eventLocation,
+    this.eventLocationLat,
+    this.eventLocationLang,
   });
-  String? id;
+  final String? eventCreatorId;
+  String? eventId;
   String? eventCategory;
   String? eventTitle;
   String? eventDescription;
   String? eventDate;
   String? eventTime;
-  String? eventLocation;
+  String? eventLocationLat;
+  String? eventLocationLang;
+
+  static EventModel empty() => EventModel();
+
+  Map<String, dynamic> toFireStore() {
+    return {
+      "EventCreatorId": eventCreatorId ?? "",
+      "EventId": eventId ?? "",
+      "EventCategory": eventCategory ?? "",
+      "EventTitle": eventTitle ?? "",
+      "EventDescription": eventDescription ?? "",
+      "EventDate": eventDate ?? "",
+      "EventTime": eventTime ?? "",
+      "EventLocationLat": eventLocationLat ?? "0",
+      "EventLocationLang": eventLocationLang ?? "0",
+    };
+  }
+
+  factory EventModel.fromFireStore(
+      DocumentSnapshot<Map<String, dynamic>> snapshot) {
+    final data = snapshot.data();
+    if (data != null) {
+      return EventModel(
+        eventCreatorId: data["EventCreatorId"] as String?,
+        eventId: data["EventId"] as String?,
+        eventCategory: data["EventCategory"] as String?,
+        eventTitle: data["EventTitle"] as String?,
+        eventDescription: data["EventDescription"] as String?,
+        eventDate: data["EventDate"] as String?,
+        eventTime: data["EventTime"] as String?,
+        eventLocationLat: data["EventLocationLat"] as String?,
+        eventLocationLang: data["EventLocationLang"] as String?,
+      );
+    } else {
+      return empty();
+    }
+  }
 }
