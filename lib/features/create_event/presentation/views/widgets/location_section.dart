b7351d66561851e2123ff95995d51d2c7a24e66e
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-05-22 22:55:16 +0300

    adding location section

---
+++ lib/features/create_event/presentation/views/widgets/location_section.dart
@@ -0,0 +1,88 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:evently_app/core/constants/app_icons.dart';
+import 'package:evently_app/core/constants/app_text.dart';
+import 'package:evently_app/features/start/presentation/views_model/start_cubit.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class LocationSection extends StatelessWidget {
+  const LocationSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final startController = BlocProvider.of<StartCubit>(context);
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          AppText.location.tr(),
+          style: Theme.of(context).textTheme.titleMedium,
+        ),
+        const RSizedBox(height: 8),
+        Container(
+          decoration: BoxDecoration(
+            color: Theme.of(context).colorScheme.primaryFixed,
+            border: Border.all(
+              color: Theme.of(context).colorScheme.primary,
+            ),
+            borderRadius: BorderRadius.circular(
+              16.r,
+            ),
+          ),
+          child: GestureDetector(
+            onTap: () {},
+            child: RPadding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  Container(
+                    alignment: Alignment.center,
+                    width: 46.r,
+                    height: 46.r,
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).colorScheme.primary,
+                      borderRadius: BorderRadius.circular(
+                        8.r,
+                      ),
+                    ),
+                    child: SvgPicture.asset(
+                      AppIcons.location,
+                      width: 22.r,
+                      height: 22.r,
+                      fit: BoxFit.cover,
+                      colorFilter: ColorFilter.mode(
+                        Theme.of(context).colorScheme.primaryFixed,
+                        BlendMode.srcIn,
+                      ),
+                    ),
+                  ),
+                  const RSizedBox(width: 8),
+                  Expanded(
+                    child: Text(
+                      AppText.chooseEventLocation.tr(),
+                      style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                            color: Theme.of(context).colorScheme.primary,
+                          ),
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ),
+                  const RSizedBox(width: 8),
+                  Icon(
+                    startController.isArLanguage
+                        ? Icons.arrow_back_ios
+                        : Icons.arrow_forward_ios,
+                    color: Theme.of(context).colorScheme.primary,
+                    size: 16,
+                  )
+                ],
+              ),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
