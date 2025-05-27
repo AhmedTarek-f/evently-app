5f7e4faf808d9d04e050b3180c81de3eef25ac51
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-05-27 17:41:53 +0300

    adding home remote data

---
+++ lib/features/home/data/data_sources/home_remote_data.dart
@@ -0,0 +1,66 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:dartz/dartz.dart';
+import 'package:evently_app/core/constants/app_text.dart';
+import 'package:evently_app/core/utils/errors/failure.dart';
+import 'package:evently_app/core/utils/errors/firebase_errors.dart';
+import 'package:evently_app/features/auth/register/data/models/user_model.dart';
+import 'package:evently_app/features/create_event/data/models/event_model.dart';
+
+abstract class HomeRemoteData {
+  static final _db = FirebaseFirestore.instance;
+  static CollectionReference<EventModel> _getEventsCollection() {
+    return _db.collection("Events").withConverter(
+          fromFirestore: (snapshot, options) =>
+              EventModel.fromFireStore(snapshot),
+          toFirestore: (event, options) => event.toFireStore(),
+        );
+  }
+
+  static CollectionReference<UserModel> _getUserCollection() {
+    return _db.collection("User").withConverter(
+          fromFirestore: (snapshot, options) =>
+              UserModel.fromFireStore(snapshot),
+          toFirestore: (user, options) => user.toFireStore(),
+        );
+  }
+
+  static Future<Either<Failure, List<EventModel>>> fetchAllEvents() async {
+    try {
+      var eventsDocument =
+          await _getEventsCollection().orderBy("EventDate").get();
+      return right(eventsDocument.docs.map((event) => event.data()).toList());
+    } catch (error) {
+      if (error is FirebaseException) {
+        return left(FirebaseErrors.firebaseExceptions(error));
+      }
+      return left(
+        FirebaseErrors(
+          errorMessage: "${AppText.unknownErrorMessage} ${error.toString()}",
+        ),
+      );
+    }
+  }
+
+  static Future<Either<Failure, void>> updateFavoriteEvents({
+    required String userId,
+    required List<String> favoriteEventsList,
+  }) async {
+    try {
+      await _getUserCollection().doc(userId).update(
+        {
+          "FavoriteEvents": favoriteEventsList,
+        },
+      );
+      return right(null);
+    } catch (error) {
+      if (error is FirebaseException) {
+        return left(FirebaseErrors.firebaseExceptions(error));
+      }
+      return left(
+        FirebaseErrors(
+          errorMessage: "${AppText.unknownErrorMessage} ${error.toString()}",
+        ),
+      );
+    }
+  }
+}
