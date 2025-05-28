c22d2f5900921f536fab2a680f9ab8e3d3481d51
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-05-28 21:46:21 +0300

    adding get event location name method

--- lib/features/create_event/data/repositories/create_event_repository.dart
+++ lib/features/create_event/data/repositories/create_event_repository.dart
@@ -2,6 +2,8 @@ import 'package:dartz/dartz.dart';
 import 'package:evently_app/core/utils/errors/failure.dart';
 import 'package:evently_app/features/create_event/data/data_sources/remote_data_sources/create_event_remote_data.dart';
 import 'package:evently_app/features/create_event/data/models/event_model.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 abstract class CreateEventRepository {
   static Future<Either<Failure, void>> createEvent({
@@ -9,6 +11,13 @@ abstract class CreateEventRepository {
   }) async {
     return await CreateEventRemoteData.createEvent(
       eventData: eventData,
+    );
+  }
+
+  static Future<Either<Failure, List<Placemark>>> getEventLocationName(
+      {required LatLng eventLocation}) async {
+    return await CreateEventRemoteData.getEventLocationName(
+      eventLocation: eventLocation,
     );
   }
 }
