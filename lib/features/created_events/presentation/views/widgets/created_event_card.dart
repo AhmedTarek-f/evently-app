eccdb3e81b70a1b6ae5951f51ae5a90599f536c3
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-06-01 22:50:31 +0300

    adding created event card

---
+++ lib/features/created_events/presentation/views/widgets/created_event_card.dart
@@ -0,0 +1,94 @@
+import 'package:evently_app/core/common_widgets/shimmer/event_image_card_shimmer.dart';
+import 'package:evently_app/core/utils/evently_methods_helper.dart';
+import 'package:evently_app/features/auth/register/data/models/user_model.dart';
+import 'package:evently_app/features/create_event/data/models/event_model.dart';
+import 'package:evently_app/features/created_events/presentation/views/widgets/created_event_card_date.dart';
+import 'package:evently_app/features/created_events/presentation/views/widgets/created_event_card_title.dart';
+import 'package:evently_app/features/event_details/presentation/views/event_details_view.dart';
+import 'package:evently_app/features/event_details/presentation/views_model/event_details_cubit.dart';
+import 'package:evently_app/features/evently_bottom_navigation/presentation/views_model/evently_bottom_navigation_cubit.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class CreatedEventCard extends StatelessWidget {
+  const CreatedEventCard({
+    super.key,
+    required this.eventData,
+  });
+  final EventModel eventData;
+  @override
+  Widget build(BuildContext context) {
+    final eventlyController =
+        BlocProvider.of<EventlyBottomNavigationCubit>(context);
+    return GestureDetector(
+      onTap: () => Navigator.of(context).push(
+        MaterialPageRoute(
+          builder: (context) => BlocProvider<EventDetailsCubit>(
+            create: (context) => EventDetailsCubit(
+              context,
+              eventData,
+              eventlyController.currentUserData ?? UserModel.empty(),
+            ),
+            child: const EventDetailsView(),
+          ),
+        ),
+      ),
+      child: Container(
+        width: ScreenUtil().screenWidth,
+        height: 203.h,
+        decoration: BoxDecoration(
+          border: Border.all(
+            color: Theme.of(context).colorScheme.primary,
+          ),
+          boxShadow: [
+            BoxShadow(
+              color: Theme.of(context).colorScheme.primary,
+              blurStyle: BlurStyle.outer,
+              blurRadius: 6.r,
+            )
+          ],
+          borderRadius: BorderRadius.circular(16.r),
+        ),
+        child: Stack(
+          children: [
+            Positioned.fill(
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(16.r),
+                child: Image.asset(
+                  EventlyMethodsHelper.getEventCategoryImage(
+                    eventCategory: eventData.eventCategory ?? "",
+                    context: context,
+                  ),
+                  fit: BoxFit.cover,
+                  frameBuilder:
+                      (context, child, frame, wasSynchronouslyLoaded) {
+                    if (frame == null) {
+                      return const EventImageCardShimmer();
+                    }
+                    return child;
+                  },
+                ),
+              ),
+            ),
+            RPadding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  CreatedEventCardDate(
+                    eventDate: eventData.eventDate ?? DateTime.now(),
+                  ),
+                  CreatedEventCardTitle(
+                    eventTitle: eventData.eventTitle,
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
