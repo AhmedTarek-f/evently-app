1c30baadc115204fa5f6d4f905f40c4fee31d923
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-06-01 22:48:54 +0300

    adding created events cubit

---
+++ lib/features/created_events/presentation/views_model/created_events_cubit.dart
@@ -0,0 +1,33 @@
+import 'package:evently_app/core/utils/evently_methods_helper.dart';
+import 'package:evently_app/features/create_event/data/models/event_model.dart';
+import 'package:evently_app/features/created_events/presentation/views_model/created_events_state.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:intl/intl.dart';
+
+class CreatedEventsCubit extends Cubit<CreatedEventsState> {
+  CreatedEventsCubit() : super(CreatedEventsInitial()) {
+    onInit();
+  }
+
+  List<EventModel> createdEventsList = [];
+
+  onInit() {
+    createdEventsList = fetchCreatedEvents();
+  }
+
+  List<String> getEventDate({required DateTime date}) {
+    String day = DateFormat("dd").format(date);
+    String month = DateFormat("MMM").format(date).substring(0, 3);
+    return <String>[day, month];
+  }
+
+  List<EventModel> fetchCreatedEvents() {
+    return EventlyMethodsHelper.allEvents
+        .where(
+          (event) =>
+              event.eventCreatorId == FirebaseAuth.instance.currentUser!.uid,
+        )
+        .toList();
+  }
+}
