29a8c6ebbfac245e6b473d18329bcd7e50efb145
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-06-01 20:17:21 +0300

    adding delete event dialog and logic

--- lib/features/event_details/presentation/views/widgets/event_details_app_bar.dart
+++ lib/features/event_details/presentation/views/widgets/event_details_app_bar.dart
@@ -1,8 +1,13 @@
+import 'package:evently_app/core/common_widgets/custom_elevated_button.dart';
+import 'package:evently_app/core/common_widgets/dialogs/dialogs.dart';
+import 'package:evently_app/core/common_widgets/loading_button.dart';
+import 'package:evently_app/core/constants/app_colors.dart';
 import 'package:evently_app/core/constants/app_icons.dart';
 import 'package:evently_app/core/constants/app_text.dart';
 import 'package:evently_app/features/edit_event/presentation/views/edit_event_view.dart';
 import 'package:evently_app/features/edit_event/presentation/views_model/edit_event_cubit.dart';
 import 'package:evently_app/features/event_details/presentation/views_model/event_details_cubit.dart';
+import 'package:evently_app/features/event_details/presentation/views_model/event_details_state.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -45,7 +50,49 @@ class EventDetailsAppBar extends StatelessWidget
                 ),
               ),
               IconButton(
-                onPressed: () {},
+                onPressed: () {
+                  Dialogs.showOptionDialog(
+                    context: context,
+                    firstButtonTitle: '',
+                    secondButtonTitle: '',
+                    optionDialogContent:
+                        "${AppText.deleteEventMessage} ${controller.eventData.eventTitle}",
+                    contentHorizontalPadding: 16,
+                    dialogButton: BlocProvider.value(
+                      value: controller,
+                      child: Row(
+                        children: [
+                          Expanded(
+                            child: CustomElevatedButton(
+                              onPressed: () => Navigator.of(context).pop(),
+                              buttonTitle: AppText.cancel,
+                            ),
+                          ),
+                          const RSizedBox(width: 16),
+                          BlocBuilder<EventDetailsCubit, EventDetailsState>(
+                            buildWhen: (previous, current) =>
+                                current is DeleteEventLoadingState,
+                            builder: (context, state) =>
+                                state is DeleteEventLoadingState
+                                    ? const Expanded(
+                                        child: LoadingButton(
+                                          backgroundColor: AppColors.red,
+                                        ),
+                                      )
+                                    : Expanded(
+                                        child: CustomElevatedButton(
+                                          onPressed: () async =>
+                                              await controller.deleteEvent(),
+                                          buttonTitle: AppText.delete,
+                                          backgroundColor: AppColors.red,
+                                        ),
+                                      ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
                 icon: SvgPicture.asset(
                   AppIcons.delete,
                   width: 24.r,
