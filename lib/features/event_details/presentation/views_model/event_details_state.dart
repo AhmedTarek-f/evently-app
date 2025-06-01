2eef1912ba4d8ed4d14acb91a836f4dc8e15e91b
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-06-01 12:19:57 +0300

    adding event details state

---
+++ lib/features/event_details/presentation/views_model/event_details_state.dart
@@ -0,0 +1,40 @@
+sealed class EventDetailsState {}
+
+final class EventDetailsInitial extends EventDetailsState {}
+
+final class CustomIconLoadState extends EventDetailsState {}
+
+final class MapStyleLoadState extends EventDetailsState {}
+
+final class FetchEventCreatorSuccessState extends EventDetailsState {}
+
+final class FetchEventCreatorLoadingState extends EventDetailsState {}
+
+final class FetchEventCreatorFailureState extends EventDetailsState {
+  FetchEventCreatorFailureState({required this.errorMessage});
+  final String errorMessage;
+}
+
+final class FetchParticipateUsersSuccessState extends EventDetailsState {}
+
+final class FetchParticipateUsersLoadingState extends EventDetailsState {}
+
+final class FetchParticipateUsersFailureState extends EventDetailsState {
+  FetchParticipateUsersFailureState({required this.errorMessage});
+  final String errorMessage;
+}
+
+final class ChangeParticipateUsersState extends EventDetailsState {}
+
+final class GetUserJoiningState extends EventDetailsState {}
+
+final class ToggleParticipateInOutEventSuccessState extends EventDetailsState {}
+
+final class ToggleParticipateInOutEventLoadingState extends EventDetailsState {}
+
+final class ToggleParticipateInOutEventFailureState extends EventDetailsState {
+  ToggleParticipateInOutEventFailureState({required this.errorMessage});
+  final String errorMessage;
+}
+
+final class EventDetailsEditedState extends EventDetailsState {}
