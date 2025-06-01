f75acb7c6dd823feca673bf036ab42d68645cd1b
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-06-01 12:19:38 +0300

    adding event details repository

---
+++ lib/features/event_details/data/repositories/event_details_repository.dart
@@ -0,0 +1,30 @@
+import 'package:dartz/dartz.dart';
+import 'package:evently_app/core/utils/errors/failure.dart';
+import 'package:evently_app/features/auth/register/data/models/user_model.dart';
+import 'package:evently_app/features/event_details/data/data_sources/remote_data/event_details_remote_data.dart';
+
+abstract class EventDetailsRepository {
+  static Future<Either<Failure, UserModel>> fetchEventCreator({
+    required String userId,
+  }) async {
+    return await EventDetailsRemoteData.fetchEventCreator(userId: userId);
+  }
+
+  static Future<Either<Failure, List<UserModel>>> fetchParticipateUsers({
+    required List<String> participateUserIds,
+  }) async {
+    return await EventDetailsRemoteData.fetchParticipateUsers(
+      participateUserIds: participateUserIds,
+    );
+  }
+
+  static Future<Either<Failure, void>> participateInEvent({
+    required String eventId,
+    required List<String> participateUserIds,
+  }) async {
+    return await EventDetailsRemoteData.participateInEvent(
+      participateUserIds: participateUserIds,
+      eventId: eventId,
+    );
+  }
+}
