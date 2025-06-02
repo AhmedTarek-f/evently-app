94c937ddc3b2588772279135df2a5ea2601ed0b1
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-06-02 17:30:12 +0300

    adding delete user account and relative events methods

--- lib/features/profile/data/repositories/profile_repository.dart
+++ lib/features/profile/data/repositories/profile_repository.dart
@@ -1,9 +1,23 @@
 import 'package:dartz/dartz.dart';
 import 'package:evently_app/core/utils/errors/failure.dart';
-import 'package:evently_app/core/utils/services/auth_services/additional_auth_services/additional_auth_services.dart';
+import 'package:evently_app/features/profile/data/data_sources/remote_data/profile_remote_data.dart';
 
 abstract class ProfileRepository {
   static Future<Either<Failure, void>> logout() async {
-    return await AdditionalAuthServices.logout();
+    return await ProfileRemoteData.logout();
+  }
+
+  static Future<Either<Failure, void>> deleteUserAccount({
+    required String userId,
+  }) async {
+    return await ProfileRemoteData.deleteUserAccount(userId: userId);
+  }
+
+  static Future<Either<Failure, void>> deleteUserRelatedEvents({
+    required String userId,
+  }) async {
+    return await ProfileRemoteData.deleteUserRelatedEvents(
+      userId: userId,
+    );
   }
 }
