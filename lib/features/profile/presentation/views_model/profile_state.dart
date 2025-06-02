fa6dbd4b07807e95eae14ce30483ef6db6cd4785
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-06-02 17:30:36 +0300

    adding delete account states

--- lib/features/profile/presentation/views_model/profile_state.dart
+++ lib/features/profile/presentation/views_model/profile_state.dart
@@ -10,3 +10,12 @@ final class LogoutFailureState extends ProfileState {
   LogoutFailureState({required this.errorMessage});
   final String errorMessage;
 }
+
+final class DeleteAccountSuccessState extends ProfileState {}
+
+final class DeleteAccountLoadingState extends ProfileState {}
+
+final class DeleteAccountFailureState extends ProfileState {
+  DeleteAccountFailureState({required this.errorMessage});
+  final String errorMessage;
+}
